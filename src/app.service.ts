de525727fdaa18b03df2cffd6899c00aaba9a25b
Сергей Блохин <sblohin@cloudfactory.ru>
2025-02-17 16:26:10 +0300

    add Ваня

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getList(): string[] {
-    return ['Cаня', 'Паша', 'Олег'];
+    return ['Cаня', 'Паша', 'Олег','Ваня'];
   }
 }
