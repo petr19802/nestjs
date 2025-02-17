01adbe1e6582e500b91914237500b00fe32104b6
Сергей Блохин <sblohin@cloudfactory.ru>
2025-02-17 17:00:36 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://91.103.252.147:3000'], // Разрешенные источники
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешенные методы
+    credentials: true, // Разрешить отправку куки и авторизационных заголовков
+  });
+  await app.listen(process.env.PORT ?? 5432);
 }
 bootstrap();
